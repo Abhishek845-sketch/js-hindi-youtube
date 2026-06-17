f1216c0cd242c7eaa59687bb22a5ae48f2d45468
Abhishek845-sketch <a9486806prasad@gmail.com>
2026-06-17 18:19:08 +0000

    stagedchanges

---
+++ 01_basics/03_conversionsoperation.js
@@ -0,0 +1,23 @@
+let score="hitesh"
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueinnumber=Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+
+
+
+//"33"==33
+// "33abc"==NaN
+// true==1;false==0
+let isloggedin="hitesh"
+let booleanisloggedin=Boolean(isloggedin)
+//console.log(booleanisloggedin);
+//1==true;0==false
+//""==false
+//"hitesh"==true
+let somenumber=33;
+let stringnumber=String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber)
